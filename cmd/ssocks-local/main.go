84407cc866bd8bca66953bf5882078f9f1cf9aff
Li Yazhou <me.ssword@gmail.com>
2018-12-31 18:12:10 +0800

    add command options

--- cmd/ssocks-local/main.go
+++ cmd/ssocks-local/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/Fleurer/go-ssocks/pkg/ssocks"
 )
 
+type CmdOptions struct {
+	ServerAddr   string
+	ListenPort   int
+	Password     string
+	CipherMethod string
+}
+
 func main() {
-	s, err := ssocks.NewLocalServer("0.0.0.0:9292", "localhost:9090", "123", "aes-128-cfb")
+	opt := CmdOptions{}
+	flag.StringVar(&opt.ServerAddr, "s", "", "server address")
+	flag.IntVar(&opt.ListenPort, "p", 0, "listen port")
+	flag.StringVar(&opt.Password, "P", "", "password")
+	flag.StringVar(&opt.CipherMethod, "m", "", "cipher method")
+	listenAddr := fmt.Sprintf("0.0.0.0:%d", opt.ListenPort)
+	s, err := ssocks.NewLocalServer(listenAddr, opt.ServerAddr, opt.Password, opt.CipherMethod)
 	if err != nil {
 		fmt.Printf("NewLocalServer error: %s\n", err)
 	}
